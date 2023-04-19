5990e2abef88fae6f1292c0c788ac4388cdf610c
Luis Matias Kühn <matiaskuhn.93@gmail.com>
2023-04-18 22:29:06 -0300

    Add project files.

---
+++ Business/B_Warehouse.cs
@@ -0,0 +1,46 @@
+﻿using DataAccess;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Business
+{
+    public class B_Wharehouse
+    {
+        public static List<WharehouseEntity> WherehouseList()
+        {
+            using (var db = new InventaryContext())
+            {
+                return db.Warehouses.ToList();
+            }
+        }        
+
+        public static void CreateWherehouse(WharehouseEntity oWarehouse)
+        {
+            using (var db = new InventaryContext())
+            {
+                db.Warehouses.Add(oWarehouse);
+                db.SaveChanges();
+            }
+        }
+
+        public static void UpdateWherehouse(WharehouseEntity oWarehouse)
+        {
+            using (var db = new InventaryContext())
+            {
+                db.Warehouses.Update(oWarehouse);
+                db.SaveChanges();
+            }
+        }
+
+        public static WharehouseEntity WhareHouseByID(string id)
+        {
+            using (var db = new InventaryContext())
+            {
+                return db.Warehouses.ToList().LastOrDefault(whareHouse => whareHouse.WarehouseId == id);
+            }
+        }
+    }
+}
---
+++ DataAccess/InventaryContext.cs
@@ -0,0 +1,30 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess
+{
+    public class InventaryContext : DbContext
+    {
+        public DbSet<ProductEntity> Products { get; set; }
+        public DbSet<CategoryEntity> Categories { get; set; }
+        public DbSet<InputOutputEntity> InOuts { get; set; }
+        public DbSet<WharehouseEntity> Warehouses { get; set; }
+        public DbSet<StorageEntity> Storages { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            if (!options.IsConfigured)
+            {
+                options.UseSqlServer(@"Server=DESKTOP-KV6V470\SQLEXPRESS;Database=PlatziCORE;User Id=sa;Password=12345;Trusted_Connection=True;TrustServerCertificate=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
